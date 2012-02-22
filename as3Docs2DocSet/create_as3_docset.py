23e46e1ec35ca5e81d00607036cfcb58efce7bbd
Mark Grandi <markgrandi@gmail.com>
2012-02-22 15:40:16 -0700

    added the bs4 library, and started making the script work under python 3, have it using argparse and the 'type' argument for add_argument so it can do verification, etc.

--- as3Docs2DocSet/create_as3_docset.py
+++ as3Docs2DocSet/create_as3_docset.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 #
 # forked from https://github.com/gpambrozio/PythonScripts
 #
 # Edited script to create a .docset for the as3/flex documentation
+# Note that its now in python 3.
 # 
 # edited by Mark Grandi
 # 2/21/2012
@@ -12,52 +13,48 @@
 
 import re
 import os
+import os.path
 import shutil
 import subprocess
 from bs4 import BeautifulSoup
+import argparse
 
-## Tries to find docsetutil
-possible_docsetutil_path = [
-    "/Developer/usr/bin/docsetutil",
-    "/Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil",
-]
-docsetutil_path = [path for path in possible_docsetutil_path if os.path.exists(path)]
-if len(docsetutil_path) == 0:
-    print "Could not find docsetutil. Please check for docsetutil's location and set it inside the script."
-    exit(1)
 
-docsetutil_path = docsetutil_path[0]
 
-## Script should run in the folder where the docs live
-source_folder = os.getcwd() + "/"
+def verify_docpath(argString):
+    ''' this method is the 'type' of the docPath argument, and this is called
+    to 'verify' the docpath path, to make sure that there is the documentation at the specified location
+    @param argString - the string that the ArgumentParser got from the command line
+    @return a string, the same string that it encountered or throws an error if this path isn't the as3 docpath'''
 
-# destination folder
-dest_folder = source_folder + "as3.docset/" 
 
-# make sure we are in the right folder, search for "ActionScript&reg; 3.0 Reference for the Adobe&reg; Flash&reg; Platform"
-# in index.html
-try:
-    with open("index.html", "r") as f:
+    # make sure the path exists
+    if not os.path.exists(argString):
 
-        success = False
+        raise argparse.ArgumentTypeError("the path specified does not exist")
 
-        # see if we can find that line. if we do, break out of the loop and keep going. if not, print error and exit
-        for line in f:
-            search = re.search("ActionScript&reg; 3.0 Reference for the Adobe&reg; Flash&reg; Platform", line)
+    # make sure we are in the right folder, search for "ActionScript&reg; 3.0 Reference for the Adobe&reg; Flash&reg; Platform"
+    # in index.html
+    try:
+        with open(os.path.join(argString, "index.html"), "r") as f:
 
-            if search:
-                success = True
-                break
-        if not success:
-            print("This doesn't seem to be the actionscript 3 documentation, are you in the right folder?")
-            sys.exit(1)
+            success = False
 
-except IOError:
+            # see if we can find that line. if we do, break out of the loop and keep going. if not, print error and exit
+            for line in f:
+                search = re.search("ActionScript&reg; 3.0 Reference for the Adobe&reg; Flash&reg; Platform", line)
 
-    print("Could not find index.html, are you in the right folder?")
-    sys.exit(1)
+                if search:
+                    success = True
+                    break
+            if not success:
+                raise argparse.ArgumentTypeError("This doesn't seem to be the actionscript 3 documentation, are you in the right folder?")
+                
 
-    
+    except IOError:
+
+        raise argparse.ArgumentTypeError("Could not find index.html, are you in the right folder?")
+
 
 
 def is_something(tag, something):
@@ -72,159 +69,217 @@ def collect(soup, what, identifier, names):
     """ Collects all nodes of a certain type from a BeautifulSoup document """
     whats = soup.find_all(lambda tag: is_something(tag, what))
     for n in whats:
-        apple_ref = "//apple_ref/cpp/%s/%s" % (identifier, n["id"])
+        apple_ref = "//apple_ref/cpp/{}/{}".format(identifier, n["id"])
         new_tag = soup.new_tag("a")
         new_tag['name'] = apple_ref
         n.insert_before(new_tag)
         names.append(apple_ref)
 
+def trouble(message):
+    ''' prints an error message and exits with status 1
+    @param message - the error message'''
 
-## Clean up first
-if os.path.exists(dest_folder):
-    shutil.rmtree(dest_folder)
+    print(message)
+    sys.exit(1)
 
-## Create all the necessary folder hierarchy
-os.makedirs(dest_folder + "Contents/Resources/Documents/")
-docset_folder = dest_folder
-dest_folder = dest_folder + "Contents/"
+def makeDocset(args):
+    ''' does the work to make the docset
+        @param args - the argument parser namespace object
+        '''
 
-## Find the module's index file. this is probably the as3's class index
-possible_modindex_path = [
-    "package-list.html"
-]
-modindex_path = [path for path in possible_modindex_path if os.path.exists(source_folder + path)]
-if len(modindex_path) == 0:
-    print "Could not find the as3 package index. Please check your doc folder structure and try again."
-    exit(2)
-modindex_path = modindex_path[0]
+    ## Tries to find docsetutil
+    possible_docsetutil_path = [
+        "/Developer/usr/bin/docsetutil",
+        "/Applications/Xcode.app/Contents/Developer/usr/bin/docsetutil",
+    ]
+    docsetutil_path = [path for path in possible_docsetutil_path if os.path.exists(path)]
+    if len(docsetutil_path) == 0:
+        trouble("Could not find docsetutil. Please check for docsetutil's location and set it inside the script.")
 
-## Create Info.plist
-with open(dest_folder + "Info.plist", "w") as info:
-    info.write("""<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>CFBundleIdentifier</key>
-        <string>as3</string>
-        <key>CFBundleName</key>
-        <string>Actionscript 3</string>
-        <key>DocSetPlatformFamily</key>
-        <string>as3</string>
-    </dict>
-    </plist>
+    docsetutil_path = docsetutil_path[0]
+
+    ## Script should run in the folder where the docs live
+    source_folder = os.getcwd()
+
+    # destination folder. this changes throughout the script.
+    dest_folder = os.path.join(source_folder,"as3.docset")
+
+
+    ## Clean up first if the output folders already exist
+    if os.path.exists(dest_folder):
+        shutil.rmtree(dest_folder)
+
+    ## Create all the necessary folder hierarchy
+    os.makedirs(dest_folder + "Contents/Resources/Documents/")
+    docset_folder = dest_folder
+    dest_folder = os.path.join(dest_folder, "Contents")
+
+    ## Find the module's index file. this is probably the as3's class index
+    possible_modindex_path = [
+        "package-list.html"
+    ]
+    modindex_path = [path for path in possible_modindex_path if os.path.exists(source_folder + path)]
+    if len(modindex_path) == 0:
+        trouble("Could not find the as3 package index. Please check your doc folder structure and try again.")
+
+    modindex_path = modindex_path[0]
+
+    ## Create Info.plist
+    with open(dest_folder + "Info.plist", "w") as info:
+        info.write("""<?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>CFBundleIdentifier</key>
+            <string>as3</string>
+            <key>CFBundleName</key>
+            <string>Actionscript 3</string>
+            <key>DocSetPlatformFamily</key>
+            <string>as3</string>
+        </dict>
+        </plist>
+        """)
+
+    ## Create Nodes.xml
+    dest_folder = os.path.join(dest_folder , "Resources")
+    with open(dest_folder + "Nodes.xml", "w") as nodes:
+        nodes.write("""<?xml version="1.0" encoding="UTF-8"?>
+        <DocSetNodes version="1.0">
+            <TOC>
+                <Node type="folder">
+                    <Name>Package Index</Name>
+                    <Path>{}</Path>
+                </Node>
+            </TOC>
+        </DocSetNodes>
+        """.format(modindex_path))
+
+
+
+    dest_folder = os.path.join(dest_folder ,"Documents")
+
+    # copy the entire langref folder over
+    shutil.copytree(source_folder, dest_folder)
+
+    ## I'll hide the header because it makes no sense in a docset
+    ## and messes up Dash
+    ## TODO make edits to the css file! not these though, these are for the python docs
+    '''
+    css = open(dest_folder + "_static/basic.css", "a+")
+    css.write("div.related {display:none;}\n")
+    css.close()
+    css = open(dest_folder + "_static/default.css", "a+")
+    css.write("a.headerlink {display:none;}\n")
+    css.close()
+
+    ## Start of the tokens file
+    tokens.write("""<?xml version="1.0" encoding="UTF-8"?>
+    <Tokens version="1.0">
     """)
+    '''
 
-## Create Nodes.xml
-dest_folder = dest_folder + "Resources/"
-with open(dest_folder + "Nodes.xml", "w") as nodes:
-    nodes.write("""<?xml version="1.0" encoding="UTF-8"?>
-    <DocSetNodes version="1.0">
-        <TOC>
-            <Node type="folder">
-                <Name>Package Index</Name>
-                <Path>%s</Path>
-            </Node>
-        </TOC>
-    </DocSetNodes>
-    """ % modindex_path)
+    htmlPagesToParse = ["all-index-A.html",
+                        "all-index-B.html",
+                        "all-index-C.html",
+                        "all-index-D.html",
+                        "all-index-E.html",
+                        "all-index-F.html",
+                        "all-index-G.html",
+                        "all-index-H.html",
+                        "all-index-I.html",
+                        "all-index-J.html",
+                        "all-index-K.html",
+                        "all-index-L.html",
+                        "all-index-M.html",
+                        "all-index-N.html",
+                        "all-index-O.html",
+                        "all-index-P.html",
+                        "all-index-Q.html",
+                        "all-index-R.html",
+                        "all-index-S.html",
+                        "all-index-T.html",
+                        "all-index-U.html",
+                        "all-index-V.html",
+                        "all-index-W.html",
+                        "all-index-X.html",
+                        "all-index-Y.html",
+                        "all-index-Z.html",
+                        "all-index-Symbols.html"]
+
+    # dictionary that will hold the pages
+    # key is the html files path, and value is a list of 
+    # strings that will will be of the format //apple_ref/language/type/name
+    # that identifies the various classes, properties, styles, etc inside each html file.
+    pages = {}
+
+    for htmlFile in htmlPagesToParse:
+
+        # we are in the 'langref' folder, and everything is in there
+        with open(htmlFile, "r") as f:
+
+            # create the soup
+            soup = BeautifulSoup(f)
+
+            # get all the <td> tags that have the class name "idxrow", which contains as a child stuff we want. 
+            tmpList = soup.find_all("td", {"class": "idxrow"})
+
+            for tag in tmpList:
+
+                print(tag.string)
 
 
-## Create the tokens file
-tokens = open(dest_folder + "Tokens.xml", "w")
-dest_folder = dest_folder + "Documents/"
 
-## Copy some static files
-# markedit i probably need to copy everything.... check on that though
-shutil.copy(source_folder + "searchindex.js", dest_folder)
-shutil.copy(source_folder + modindex_path, dest_folder)
-shutil.copy(source_folder + "genindex-all.html", dest_folder)
-shutil.copy(source_folder + "library/index.html", dest_folder)
-shutil.copytree(source_folder + "_images", dest_folder + "_images")
-shutil.copytree(source_folder + "_static", dest_folder + "_static")
+    '''
 
-## I'll hide the header because it makes no sense in a docset
-## and messes up Dash
-css = open(dest_folder + "_static/basic.css", "a+")
-css.write("div.related {display:none;}\n")
-css.close()
-css = open(dest_folder + "_static/default.css", "a+")
-css.write("a.headerlink {display:none;}\n")
-css.close()
+    ## Now write to tokens
 
-## Start of the tokens file
-tokens.write("""<?xml version="1.0" encoding="UTF-8"?>
-<Tokens version="1.0">
-""")
+    ## Create the tokens file
+    tokens = open(dest_folder + "Tokens.xml", "w")
+    for href, names in pages.items():
 
-## Collect pages first
-pages = {}
+        soup = BeautifulSoup(open(source_folder + href))
 
-## Collect pages from the package index
-f = open(source_folder + modindex_path, 'r')
-for line in f:
-    search = re.search("<a href=\"(.*)#.*?\"><tt class=\"xref\">(.*?)</tt>", line)
-    if search:
-        href = search.group(1)
-        name = search.group(2)
-        if not href in pages:
-            pages[href] = []
+        collect(soup, "class", "cl", names) # need to figure out what these do
+        collect(soup, "method", "clm", names)
+        collect(soup, "classmethod", "clm", names)
+        collect(soup, "function", "func", names)
+        collect(soup, "exception", "cl", names)
+        collect(soup, "attribute", "instp", names)
 
-        apple_ref = "//apple_ref/cpp/cat/%s" % name # add entry for category (aka python module)
-        pages[href].append(apple_ref)
+        if len(names) > 0:
+            tokens.write("<File path=\"%s\">\n" % href) # each href,names pair is a file. The "file" is the href
+            for name in names:
+                tokens.write("\t<Token><TokenIdentifier>%s</TokenIdentifier><Anchor>%s</Anchor></Token>\n" % (name, name))
+            tokens.write("</File>\n") # the names are the things inside each html file, classes, functions, etc
 
-f.close()
+            newFile = dest_folder + href
+            if not os.path.exists(os.path.dirname(newFile)):
+                os.makedirs(os.path.dirname(newFile))
+            newFile = open(newFile, "w")
+            newFile.write(str(soup))
+            newFile.close()
 
-## Collect pages from the general index
-f = open(source_folder + "genindex-all.html", 'r')
-for line in f:
-    for search in re.finditer("(<dt>|, )<a href=\"([^#]+).*?\">", line):
-        href = search.group(2)
-        if not href in pages:
-            pages[href] = [] # NOTE FOR THESE , stuff gets added in the collect method, see below in the for in loop,
-                            # we just create the entry in the dictionary with the url and an empty list if its not there
+    tokens.write("</Tokens>")
+    tokens.close()
 
-f.close()
+    subprocess.call([docsetutil_path, "index", docset_folder])
 
-## Collect pages from the library index
-f = open(source_folder + "library/index.html", 'r')
-for line in f:
-    for search in re.finditer("<a class=\"reference external\" href=\"([^#\"]+).*?\">", line):
-        href = "library/" + search.group(1)
-        if not ("http://" in href or "https://" in href or href in pages):
-            pages[href] = [] # NOTE FOR THESE , stuff gets added in the collect method, see below in the for in loop
-                            # we just create the entry in the dictionary with the url and an empty list if its not there
-f.close()
+    ## Cleanup
+    os.remove(docset_folder + "Contents/Resources/Nodes.xml")
+    os.remove(docset_folder + "Contents/Resources/Tokens.xml")
+    '''
 
-## Now write to tokens
-for href, names in pages.items():
 
-    soup = BeautifulSoup(open(source_folder + href))
+if __name__ == "__main__":
+    # if we are being run as a real program
 
-    collect(soup, "class", "cl", names) # need to figure out what these do
-    collect(soup, "method", "clm", names)
-    collect(soup, "classmethod", "clm", names)
-    collect(soup, "function", "func", names)
-    collect(soup, "exception", "cl", names)
-    collect(soup, "attribute", "instp", names)
+    parser = argparse.ArgumentParser(description="create a .docset file for the as3 documentation", 
+        epilog="Copyright 2012 Mark Grandi, forked from https://github.com/gpambrozio/PythonScripts")
 
-    if len(names) > 0:
-        tokens.write("<File path=\"%s\">\n" % href) # each href,names pair is a file. The "file" is the href
-        for name in names:
-            tokens.write("\t<Token><TokenIdentifier>%s</TokenIdentifier><Anchor>%s</Anchor></Token>\n" % (name, name))
-        tokens.write("</File>\n") # the names are the things inside each html file, classes, functions, etc
+    # optional arguments, if specified these are the input and output files, if not specified, it uses stdin and stdout
+    parser.add_argument('docPath', help="the directory where the as3 documentation is located", type=verify_docpath)
+    
+    parser.add_argument('outputPath', help="the directory to place the resulting .docset ")
+    args = parser.parse_args()
 
-        newFile = dest_folder + href
-        if not os.path.exists(os.path.dirname(newFile)):
-            os.makedirs(os.path.dirname(newFile))
-        newFile = open(newFile, "w")
-        newFile.write(str(soup))
-        newFile.close()
-
-tokens.write("</Tokens>")
-tokens.close()
-
-subprocess.call([docsetutil_path, "index", docset_folder])
-
-## Cleanup
-os.remove(docset_folder + "Contents/Resources/Nodes.xml")
-os.remove(docset_folder + "Contents/Resources/Tokens.xml")
+    makeDocset(args)
